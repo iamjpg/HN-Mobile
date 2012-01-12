b2024734fb4538ab81e2ca25be77f357df9e9667
J.P. Given <jgiven@gmail.com>
2012-01-12 12:12:21 -0800

    Completely removing railway.js to use express.js instead.

--- config/routes.js
+++
@@ -1,7 +0,0 @@
-exports.routes = function (map) {
-	map.get('/','welcome#index');
-	map.get('/search/frontpage','search#frontpage');
-	
-	map.get('/story/contents', 'story#contents');
-	map.get('/story/contents/:url', 'story#contents');
-};
